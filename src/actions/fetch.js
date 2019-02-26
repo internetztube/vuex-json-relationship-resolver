0c2c67f4241a614d9cfd409acc15a6e80e3070df
Frederic Köberl <frederic.koeberl@gmail.com>
2019-02-26 09:59:42 +0100

    Add support for custom http client

--- src/actions/fetch.js
+++ src/actions/fetch.js
@@ -4,30 +4,41 @@ const fetchMethod = (context, customHelpers, endpoint) => {
   return new Promise((resolve, reject) => {
     context.commit('setObjectLoading', endpoint)
     let headers = {}
-    if (customHelpers.hasOwnProperty('requestOptions')) headers = customHelpers.requestOptions(context.rootState, { endpoint })
+    if (customHelpers.hasOwnProperty('requestOptions')) headers = customHelpers.requestOptions(context.rootState, {endpoint})
 
-    fetch(endpoint, headers)
-      .then((response) => {
-        if (!response.ok) {
-          context.commit('setObjectDoneLoading', endpoint)
-          if (customHelpers.hasOwnProperty('onRequestFailed')) return customHelpers.onRequestFailed(context.rootState, { endpoint, response })
-          return
-        }
-        return response.json().then(body => {
-          context.commit('setObjectDoneLoading', endpoint)
-          if (Array.isArray(body.data)) {
-            const result = []
-            for (let i = 0; i < body.data.length; i++) {
-              result.push(handleObject(body.data[i], context))
-            }
-            return result
-          }
-          return handleObject(body.data, context)
+    let promise = (endpoint) => {
+      return new Promise((resolve, reject) => {
+        fetch(endpoint, headers).then((response) => {
+          if (!response.ok) reject(response)
+          return response.json().then(body => {
+            resolve(body);
+          });
         })
+      });
+    }
+
+    if (customHelpers.hasOwnProperty('httpClient')) {
+      promise = customHelpers.httpClient;
+    }
+
+    promise(endpoint).then(body => {
+      context.commit('setObjectDoneLoading', endpoint)
+      if (customHelpers.hasOwnProperty('onRequestFailed')) return customHelpers.onRequestFailed(context.rootState, {
+        endpoint,
+        response
       })
-      .catch((response) => {
-        if (customHelpers.hasOwnProperty('onRequestNetworkError')) customHelpers.onRequestNetworkError(context.rootState, { endpoint, response })
-      })
+      context.commit('setObjectDoneLoading', endpoint)
+      if (Array.isArray(body.data)) {
+        const result = []
+        for (let i = 0; i < body.data.length; i++) {
+          result.push(handleObject(body.data[i], context))
+        }
+        return result
+      }
+      return handleObject(body.data, context)
+    }).catch(response => {
+      context.commit('setObjectDoneLoading', endpoint)
+    })
   })
 }
 
