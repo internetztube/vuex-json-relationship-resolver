f251212eb31ee01e53e5ee6c7b8c2dfcc97bef5e
Frederic Köberl <frederic.koeberl@gmail.com>
2019-02-26 20:16:10 +0100

    Add support for external object building

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import module from './module'
 import mapObject from './mapper/mapObject'
 import mapObjectLoading from './mapper/mapObjectLoading'
+import handleObject from './actions/handle-object';
 
 export {
   module,
   mapObject,
-  mapObjectLoading
+  mapObjectLoading,
+  handleObject
 }
