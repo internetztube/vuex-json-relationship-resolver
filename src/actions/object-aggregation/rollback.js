41c616484ec03c2fcea3b540ff2913cf4c5b875a
Frederic Köberl <frederic.koeberl@gmail.com>
2019-03-02 13:49:50 +0100

    fix rollback

--- src/actions/object-aggregation/rollback.js
+++ src/actions/object-aggregation/rollback.js
@@ -2,9 +2,9 @@ import handleObject from '../handle-object'
 
 const rollback = ({ object, context }) => {
   object._restoreOriginalStore = JSON.stringify(object)
-  object.rollback = () => {
+  object.rollback = function () {
     // eslint-disable-next-line no-use-before-define
-    const data = JSON.parse(object._restoreOriginalStore)
+    const data = JSON.parse(this._restoreOriginalStore)
     const object = handleObject(data, context)
     return object
   }
