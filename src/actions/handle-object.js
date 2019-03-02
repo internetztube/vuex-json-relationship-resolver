c56ef22ad9aa62a6b9b4dedb80d523b25b5f8978
Frederic Köberl <frederic.koeberl@gmail.com>
2019-03-02 14:16:34 +0100

    Fix store.

--- src/actions/handle-object.js
+++ src/actions/handle-object.js
@@ -2,14 +2,16 @@ import objectRelationshipAggregator from './object-aggregation/relationship'
 import objectAttributesGetterSetter from './object-aggregation/attributes-getter-setter'
 import objectRollback from './object-aggregation/rollback'
 import storeObject from './store-object'
+import objectStore from './object-aggregation/store'
 
 const handleObject = (object, context) => {
-  object = objectRollback({ object, context }) // must be first in chain!
-  object = objectRelationshipAggregator({ object, context })
-  object = objectAttributesGetterSetter({ object, context })
+  object = objectRollback({object, context}); // must be first in chain!
+  object = objectRelationshipAggregator({object, context});
+  object = objectAttributesGetterSetter({object, context});
+  object = objectStore({object, context});
 
-  object = storeObject({ object, context })
+  object = storeObject({object, context});
   return object
-}
+};
 
 export default handleObject
